a3cd6dfc7da5d109847215b252b07448fe30cd70
Viorel Gheba <viorel.gheba@emag.ro>
2017-09-10 02:51:50 +0300

    Fix Heroku params

--- src/AppBundle/HerokuConfig.php
+++ src/AppBundle/HerokuConfig.php
@@ -20,20 +20,32 @@ class HerokuConfig
      */
     private static function populateDatabaseEnvironment(Event $event)
     {
+        $io = $event->getIO();
+
         $url = getenv('DATABASE_URL');
-
-        if ($url) {
-            $config = parse_url($url);
-
-            putenv(sprintf('DATABASE_HOST=%s', $config['host']));
-            putenv(sprintf('DATABASE_PORT=%s', $config['port']));
-            putenv(sprintf('DATABASE_USER=%s', $config['user']));
-            putenv(sprintf('DATABASE_PASSWORD=%s', $config['pass']));
-            putenv(sprintf('DATABASE_NAME=%s', ltrim('/', $config['path'])));
+        if (!$url) {
+            $io->write('DATABASE_URL env variable not found');
+            return;
         }
 
-        $io = $event->getIO();
         $io->write(sprintf('DATABASE_URL=%s', $url));
+
+        $config = parse_url($url);
+
+        putenv(sprintf('DATABASE_HOST=%s', $config['host']));
+        $io->write(sprintf('DATABASE_HOST=%s', $config['host']));
+
+        putenv(sprintf('DATABASE_PORT=%s', $config['port']));
+        $io->write(sprintf('DATABASE_PORT=%s', $config['port']));
+
+        putenv(sprintf('DATABASE_USER=%s', $config['user']));
+        $io->write(sprintf('DATABASE_USER=%s', $config['user']));
+
+        putenv(sprintf('DATABASE_PASSWORD=%s', $config['pass']));
+        $io->write(sprintf('DATABASE_PASSWORD=%s', $config['pass']));
+
+        putenv(sprintf('DATABASE_NAME=%s', ltrim('/', $config['path'])));
+        $io->write(sprintf('DATABASE_NAME=%s', ltrim('/', $config['path'])));
     }
 
     /**
@@ -41,19 +53,31 @@ class HerokuConfig
      */
     private static function populateAmqpEnvironment(Event $event)
     {
+        $io = $event->getIO();
+
         $url = getenv('AMQP_URL');
-
-        if ($url) {
-            $config = parse_url($url);
-
-            putenv(sprintf('AMQP_HOST=%s', $config['host']));
-            putenv(sprintf('AMQP_PORT=%s', $config['port']));
-            putenv(sprintf('AMQP_USER=%s', $config['user']));
-            putenv(sprintf('AMQP_PASSWORD=%s', $config['pass']));
-            putenv(sprintf('AMQP_VHOST=%s', ltrim('/', $config['path'])));
+        if (!$url) {
+            $io->write('AMQP_URL env variable not found');
+            return;
         }
 
-        $io = $event->getIO();
         $io->write(sprintf('AMQP_URL=%s', $url));
+
+        $config = parse_url($url);
+
+        putenv(sprintf('AMQP_HOST=%s', $config['host']));
+        $io->write(sprintf('AMQP_HOST=%s', $config['host']));
+
+        putenv(sprintf('AMQP_PORT=%s', $config['port']));
+        $io->write(sprintf('AMQP_PORT=%s', $config['port']));
+
+        putenv(sprintf('AMQP_USER=%s', $config['user']));
+        $io->write(sprintf('AMQP_USER=%s', $config['user']));
+
+        putenv(sprintf('AMQP_PASSWORD=%s', $config['pass']));
+        $io->write(sprintf('AMQP_PASSWORD=%s', $config['pass']));
+
+        putenv(sprintf('AMQP_VHOST=%s', ltrim('/', $config['path'])));
+        $io->write(sprintf('AMQP_VHOST=%s', ltrim('/', $config['path'])));
     }
 }
