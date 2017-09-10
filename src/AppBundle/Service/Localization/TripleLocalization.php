1884b28533e9d92c7e665e5a4146d99d9315c2e0
Viorel Gheba <viorel.gheba@emag.ro>
2017-09-10 10:28:34 +0300

    Correct division by zero

--- src/AppBundle/Service/Localization/TripleLocalization.php
+++ src/AppBundle/Service/Localization/TripleLocalization.php
@@ -58,43 +58,55 @@ class TripleLocalization extends AbstractLocalizationService
             abs($b3->getSignalStrength()) / self::CALIBRATE_DISTANCE
         );
 
-        $x = ($b2->getX() - $b3->getX())
+        $f = (
+            2
             * (
-                (pow($b2->getX(), 2) - pow($b1->getX(), 2))
-                + (pow($b2->getY(), 2) - pow($b1->getY(), 2))
-                /*+ (pow($b1->getSignalStrength(), 2) - pow($b2->getSignalStrength(), 2))*/
+                ($b1->getY() - $b2->getY()) * ($b2->getX() - $b3->getX()) - ($b2->getY() - $b3->getY()) * ($b1->getX() - $b2->getX())
             )
-            - ($b1->getX() - $b2->getX())
-            * (
-                (pow($b3->getX(), 2) - pow($b2->getX(), 2))
-                + (pow($b3->getY(), 2) - pow($b2->getY(), 2))
-                /*+ (pow($b2->getSignalStrength(), 2) - pow($b3->getSignalStrength(), 2))*/
-            )
-            / (
-                2
-                * (
-                    ($b1->getY() - $b2->getY()) * ($b2->getX() - $b3->getX()) - ($b2->getY() - $b3->getY()) * ($b1->getX() - $b2->getX())
-                )
-            );
+        );
 
-        $y = ($b2->getY() - $b3->getY())
-            * (
-                (pow($b2->getY(), 2) - pow($b1->getY(), 2))
-                + (pow($b2->getX(), 2) - pow($b1->getX(), 2))
-                /*+ (pow($b1->getSignalStrength(), 2) - pow($b2->getSignalStrength(), 2))*/
-            )
-            - ($b1->getY() - $b2->getY())
-            * (
-                (pow($b3->getY(), 2) - pow($b2->getY(), 2))
-                + (pow($b3->getX(), 2) - pow($b2->getX(), 2))
-                /*+ (pow($b2->getSignalStrength(), 2) - pow($b3->getSignalStrength(), 2))*/
-            )
-            / (
-                2
+        $x = $beacons[0]->getPositionOx();
+
+        if ($f != 0) {
+            $x = ($b2->getX() - $b3->getX())
                 * (
-                    ($b1->getX() - $b2->getX()) * ($b2->getY() - $b3->getY()) - ($b2->getX() - $b3->getX()) * ($b1->getY() - $b2->getY())
+                    (pow($b2->getX(), 2) - pow($b1->getX(), 2))
+                    + (pow($b2->getY(), 2) - pow($b1->getY(), 2))
+                    /*+ (pow($b1->getSignalStrength(), 2) - pow($b2->getSignalStrength(), 2))*/
                 )
-            );
+                - ($b1->getX() - $b2->getX())
+                * (
+                    (pow($b3->getX(), 2) - pow($b2->getX(), 2))
+                    + (pow($b3->getY(), 2) - pow($b2->getY(), 2))
+                    /*+ (pow($b2->getSignalStrength(), 2) - pow($b3->getSignalStrength(), 2))*/
+                )
+                / $f;
+        }
+
+        $f = (
+            2
+            * (
+                ($b1->getX() - $b2->getX()) * ($b2->getY() - $b3->getY()) - ($b2->getX() - $b3->getX()) * ($b1->getY() - $b2->getY())
+            )
+        );
+
+        $y = $beacons[0]->getPositionOy();
+
+        if ($f != 0) {
+            $y = ($b2->getY() - $b3->getY())
+                * (
+                    (pow($b2->getY(), 2) - pow($b1->getY(), 2))
+                    + (pow($b2->getX(), 2) - pow($b1->getX(), 2))
+                    /*+ (pow($b1->getSignalStrength(), 2) - pow($b2->getSignalStrength(), 2))*/
+                )
+                - ($b1->getY() - $b2->getY())
+                * (
+                    (pow($b3->getY(), 2) - pow($b2->getY(), 2))
+                    + (pow($b3->getX(), 2) - pow($b2->getX(), 2))
+                    /*+ (pow($b2->getSignalStrength(), 2) - pow($b3->getSignalStrength(), 2))*/
+                )
+                / $f;
+        }
 
         $this->generateNewEvent($eventDto, $beacons[0], $x, $y);
     }
