1a1fcb7ac493c967a509f0677d6b5493d30ff3aa
Lucian Dobre <kempes406@yahoo.com>
2017-09-10 03:01:50 +0300

    update sql dump uuid field

--- app/db/btracker.sql
+++ app/db/btracker.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `beacon`;
 
 CREATE TABLE `beacon` (
     `id`       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `uuid`     VARCHAR(255)     NOT NULL,
+    `uuid`     BINARY(16)     NOT NULL,
     `created`  TIMESTAMP        NULL     DEFAULT CURRENT_TIMESTAMP,
     `modified` TIMESTAMP        NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `status`   TINYINT(1)       NOT NULL DEFAULT '1',
